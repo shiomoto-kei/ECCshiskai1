0feab0935b06c0b2e19d25111b87e2e6ec2f19e5
kira1211-code <kira12110626@gmail.com>
2026-04-24 13:44:34 +0900

    安否送信画面のたたき

--- main.php
+++ main.php
@@ -1,2 +1,46 @@
 <?php
-include 'dummy';
+include 'db.php';
+
+
+$search = filter_input(INPUT_GET,/*YESボタンの変数 */"dummy");
+$point = filter_input(INPUT_GET,/*NOボタンの変数*/);
+try {
+    //SQL分実行
+    $sql = "SELECT * FROM dummy";
+    $where = "";
+
+    if($search == 1){
+        $where = "";
+    }else if($point == 2){
+
+    }
+
+
+    
+    //SQL実行結果の処理
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //PDOオブジェクトの破棄
+    $stmt = null;
+    $db = null;
+
+
+    }catch (PDOException $dummy){
+        exit("DBエラー" . $dummy->getMessage());
+    }
