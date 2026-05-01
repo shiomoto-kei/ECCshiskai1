b6d64461ac985d598b54bfa83fde48788620fcc7
shiomoto-kei <sk1918ds@gmail.com>
2026-05-01 15:29:07 +0900

    微修正

--- anpi_PHP/rogin1.php
+++ anpi_PHP/rogin1.php
@@ -5,21 +5,22 @@
 //db接続
 include 'db.php';
 session_start();
-$input_id=$_POST['user_id']??"";
-$input_pass=$_POST['user_pass']??"";
+$input_id=$_POST['username']??"";
+$input_pass=$_POST['password']??"";
 
+//
 if(!empty($input_id) && !empty($input_pass)){
     try{
         //SQL分 [:id]の部分はあとでもらってきたIDが入る
-        $sql=db->prepare("SECT * FROM userid WHRE id = :id");
+        $sql=db->prepare("SELECT * FROM username WHERE id = :id");
          
-        //ここでさっきの[:id]に[$inpot_id]が入る
-        $stmt->bindparam(':id',$inpot_id);
+        //ここでさっきの[:id]に[$input_id]が入る
+        $stmt->bindparam(':id',$input_id);
         $stmt->execute();
         $userid=$stmt->fetch();
         if($userid){
             //ここで入力されたユーザーID と、それに紐づけているパスワードが等しいかの判定。
-            if(password_verify($input_pass,$userid['user_pass'])){
+            if(password_verify($input_pass,$userid['password'])){
 
 
             //もし管理者IDだったら遷移先を変更
