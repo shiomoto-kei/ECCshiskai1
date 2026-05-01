f94ef6bd250703a020eb67d8690c272f89dba4b9
kira1211-code <kira12110626@gmail.com>
2026-05-01 16:07:38 +0900

    ダイアログ表示の修正

--- anpi_PHP/main.php
+++ anpi_PHP/main.php
@@ -2,7 +2,7 @@
 include 'db.php';
 
 $search = filter_input(INPUT_GET,/*ボタンの変数 */"dummy");
-
+$push = filter_input(INPUT_GET,"host");
 try {
     //SQL分実行
     $sql = "SELECT * FROM /*テーブルの変数*/dummy2";
@@ -17,6 +17,9 @@ try {
         $dummy -> getMessage();
     }
 
+    if($_SERVER['push'] ==='host'){
+    }
+    
     //SQL実行結果の処理
     $stmt = $db ->prepare($sql . $where);
 
@@ -25,6 +28,14 @@ try {
     }else if($search == 2){
         $stmt -> bindParam('dummy' , $search, PDO::PARAM_INT);
     }
+
+    $push = $db->prepare("SELECT E.ENAME, S.SAFETY FROM EMPLOYEE AS E
+                          JOIN SAFETY AS S ON(E.EMP_NO = S.SAFE_NO) 
+                          ");
+    $push->execute();
+    $row = $ENAME->fetch(PDO::FETCH_ASSOC);
+    //データがない場合
+    $SAFETY = $row ? $row['SAFETY'] : 'データなし';
     
     //実行結果の更新
     $dummy = "UPDATE /*安否表のdbuser名*/ SET /*安否表のdbのテーブル名*/ where id/*安否表の変数id*/ :";
