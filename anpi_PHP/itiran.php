cf15b0214b4db4d89ef61c56e2af6f62c9c211db
rikuriku0414-cpu <riku.riku0414@icloud.com>
2026-05-01 13:52:17 +0900

    仮

--- anpi_PHP/itiran.php
+++ anpi_PHP/itiran.php
@@ -45,6 +45,62 @@ if(isset($_GET["syousaiButton"])){
      exit;
 }
 
+//追加機能インサート分
+if(isset($_GET["submitTuika"])){
+    // $_SESSION['syousaiID']=$ID;
+    //  header('Location: syousai.php');
+    //  exit;
+$emp_no = $_POST['emp_no'];
+$password = $_POST['password_hash'];
+$ename = $_POST['ename'];
+$birthday = $_POST['birthday'];
+$tel = $_POST['tel'];
+$address = $_POST['address'];
+$position = $_POST['position'];
+$section= $_POST['section'];
+$jname= $_POST['jname'];
+$pname= $_POST['pname'];
+
+if(!empty($emp_no) && !empty($password) &&!empty($ename) &&!empty($birthday) &&!empty($tel) &&!empty($address) &&!empty($position) && !empty($section) && !empty($pname)&& !empty($jname)){
+try{
+
+$password_hash =$password_hash($password);
+
+      $sql_in=$db->prepare("insert into employee (emp_no,password_hash,ename,birthday,tel,address,position,section) values (:emp_no,:password_hash,:ename,:birthday,:tel,:address,:position,:section)");
+
+ $sql_in->execute([
+                ':emp_no' => $emp_no,
+                ':password_hash' =>$password_hash,
+                ':ename' =>$ename,
+                ':birthday' => $birthday,
+                ':tel' => $tel ,
+                ':address' =>$address,
+                ':position' =>$position,
+                ':section' =>$section
+
+        ]);
+
+             $sql_j=$db->prepare("insert into position (pname) values (:pname");
+            $sql_p->execute([
+                ':pname' => $pname
+            ]);
+
+            $sql_j=$db->prepare("insert into section (jname) values (:jname");
+            $sql_j->execute([
+                ':jname' => $jname
+            ]);
+
+       
+        $db->commit();
+}catch(PDOException $e){
+    
+ echo 'DBエラー'.$e ->getMessage();
+
+}
+}
+
+}
+
 
 
 ?>
@@ -73,6 +129,10 @@ if(isset($_GET["syousaiButton"])){
 
 
 <!-- 全安否情報の初期化 -->
+<div>
+    <button type="button" id="openhennsyuuBtn">編集</button>
+</div>
+
  <dialog id="dialogallsadel">
 
     <form method="get" action="itiran.php">
@@ -83,22 +143,37 @@ if(isset($_GET["syousaiButton"])){
  </dialog>
 
 
-//この処理は詳細データが表示された画面で、 userがクリックされたら走る
-// include 'db.php';
-// session_start();
-//
-//
-//クリックしたそのuserのIDを保存
-//
-//$input_syousai=クリックしたそのuserのID
-//~~~~~~~~~~~~~~
-// ~~~~~~~~~~~~~
-// ~~~~~~~~~~~~~
-// ~~~~~~~~~~~~~
-//if(!empty($input_syousai))
-// $_SESSION['syousaiID']=$input_syousai;
-// header('Location: $syousai.php');
-// exit;
+
+
+
+ <!-- 追加ボタン -->
+
+<div>
+    <button type="button" id="opentuikaBtn">＋追加</button>
+</div>
+
+
+  <dialog id="dialogtuika">
+
+    <form method="post" action="itiran.php">
+ 
+
+    名前: <input type="text" name="ename" required><br>
+    住所:<input type="text" name="address" required><br>
+    ID: <input type="text" name="emp_no" required><br>
+    電話番号: <input type="number" name="tel" required><br>
+    生年月日: <input type="text" name="birhday" required><br>
+    役職番号: <in番号t type="number" name="pname" required><br>
+    役職名: <input type="text" name="position" required><br>
+    部署番号: <input type="number" name="section" required><br>
+      部署名: <input type="text" name="jname" required><br>
+    パスワード: <input type="text" name="password_hash" required><br>
+
+    <button type="button" name="submitTuika">決定</button>
+     <button type="button" id="closeModeBtn">キャンセル</button>
+    </form>
+ </dialog>
+
 </body>
 </html>
 
