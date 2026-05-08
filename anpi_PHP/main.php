a05d6b39692394d825f537832d24832e793c7fbe
kira1211-code <kira12110626@gmail.com>
2026-05-08 15:20:25 +0900

    安否画面接続

--- anpi_PHP/main.php
+++ anpi_PHP/main.php
@@ -1,11 +1,11 @@
 <?php
 include 'db.php';
 
-$search = filter_input(INPUT_GET,/*ボタンの変数 */"dummy");
+$search = filter_input(INPUT_GET,/*ボタンの変数 */"radio-box");
 $push = filter_input(INPUT_GET,"homeBtn");
 try {
     //SQL分実行
-    $sql = "SELECT * FROM /*テーブルの変数*/dummy2";
+    $sql = "SELECT * FROM /*テーブルの変数*/SAFETY";
     $where = "";
     try{
          if($search == 1){
@@ -16,7 +16,7 @@ try {
     }catch(PDOException $dummy){
         $dummy -> getMessage();
     }
-    $sql = "SELECT * FROM /*テーブルの変数*/dummy3";
+    $sql = "SELECT * FROM /*テーブルの変数*/SAFETY";
     $where = "";
 
     if($push == 1){
@@ -27,17 +27,17 @@ try {
     $stmt = $db ->prepare($sql . $where);
 
     if($search == 1){
-        $stmt -> bindParam('dummy' , $search, PDO::PARAM_INT);
+        $stmt -> bindParam('SAFETY' , $search, PDO::PARAM_INT);
     }else if($search == 2){
-        $stmt -> bindParam('dummy' , $search, PDO::PARAM_INT);
+        $stmt -> bindParam('SAFETY' , $search, PDO::PARAM_INT);
     }
 
     if($push == 1){
-        $stmt -> bindParam('dummy' , $push,PDO::PARAM_INT);
+        $stmt -> bindParam('' , $push,PDO::PARAM_INT);
     }
     
     //実行結果の更新
-    $dummy = "UPDATE /*安否表のdbuser名*/ SET SAFERY where id SAFE_NO :";
+    $dummy = "UPDATE /*安否表のdbuser名*/ SAFETY SET SAFETY where id SAFE_NO :";
     
     $stmt ->execute();
 
