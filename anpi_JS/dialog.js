3501ae0bddf5f37d506c5b56f162d4c1ee939273
kira1211-code <kira12110626@gmail.com>
2026-05-08 14:48:06 +0900

    ダイアログからページ遷移js

---
+++ anpi_JS/dialog.js
@@ -0,0 +1,14 @@
+const modal = document.getElementById('myModal');
+document.getElementById('openDialog').onclick =() =>{
+    modal.classList.add('show');
+};
+
+document.getElementById('closeDialog').onclick = () =>{
+    modal.classList.remove('show');
+};
+
+document.getElementById('goDetail').onclick = () =>{
+    const id = document.getElementById('').textContent;
+
+    window.location.href = ``;
+};
