a94e95b1c682fe126b1e1b138fe5986c7523342f
shiomoto-kei <sk1918ds@gmail.com>
2026-05-11 17:10:08 +0900

    微修正（テストユーザー）

--- anpi_PHP/admin_syousai.php
+++ anpi_PHP/admin_syousai.php
@@ -106,6 +106,7 @@ if (isset($_GET["sakujyoButton"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>詳細(Admin)</title>
+    <link rel="stylesheet" href="../css/staffsyousai.css">
     <link rel="stylesheet" href="../css/detail.css">
 </head>
 <body>
